50625ebd75460a3962fff2750de2945469717de2
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-08-31 15:07:46 +0800

    Edit: Add Navlink behavior

---
+++ script.js
@@ -0,0 +1,25 @@
+let menuIcon = document.getElementById('menu-icon');
+let nav = document.getElementById('navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ' ]').classList.add('active');
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
